3083c2c5e362c5f2cffeba6b1213bf944235a436
董宇航 <19832364159@163.com>
2026-07-06 15:43:41 +0800

    chore: 删除临时调试脚本

--- c.py
+++
@@ -1,4 +0,0 @@
-import subprocess, os
-os.chdir(r'c:\dongyuhang\project\Bid collusion inspection')
-subprocess.run(['git','add','-A'], check=True)
-subprocess.run(['git','commit','-m','fix gpu'], check=True)
--- p.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d c:\dongyuhang\project\"Bid collusion inspection"
-git push --force
-echo done
